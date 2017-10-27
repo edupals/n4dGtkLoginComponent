cf5b39133e5eec458af8f5cfe38afe46964359a0
juanma <juanma1980@gmail.com>
2017-10-27 14:59:34 +0200

    n4d validation

--- python3-login-component/usr/share/nezumi/loginComponent/logincomponent.py
+++ python3-login-component/usr/share/nezumi/loginComponent/logincomponent.py
@@ -9,6 +9,9 @@ import json
 import cairo
 #import commands
 from gi.repository import Gtk, Gdk, GdkPixbuf, GObject, GLib, PangoCairo, Pango
+import xmlrpc.client as n4d
+import ssl
+
 import gettext
 
 gettext.textdomain('nezumi.login-component')
@@ -28,6 +31,7 @@ class loginComponent():
 		self.txt_password.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY,"badge-small")
 		self.txt_server.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY,"server")
 		self.btn_sign=Gtk.Button(label=_("Sign In"),stock=Gtk.STOCK_YES)
+		self.btn_sign.connect('clicked',self._validate)
 
 	def _set_text_for_entry(self,text,widget):
 		widget.set_placeholder_text(text)
@@ -58,7 +62,29 @@ class loginComponent():
 		self.main_box.add(frame)
 		return (self.main_box)
 
+		self.toolbar.hide()
+		self.toolbar.hide()
+
+	def _validate(self,widget=None):
+		user=self.txt_username.get_text()
+		pwd=self.txt_password.get_text()
+		print("Validating user %s %s"%(user,pwd))
+		server=self.txt_server.get_text()
+		if not server:
+			server='server'
+
+		context=ssl._create_unverified_context()
+		c = n4d.ServerProxy("https://"+server+":9779",context=context,allow_none=True)
+		ret=None
+		try:
+		    ret=c.validate_user(user,pwd)
+		except Exception as e:
+			print("ERROR %s"%e)
+			ret=[False,str(e)]
+			return
+		print("RET: %s"%ret)
+		if ret[0]:
+			self.after_validate()
+
 	def accept_signal(self,func,data=None):
-		print(data)
-		print(func)
-		self.btn_sign.connect('clicked',func)
+		self.after_validate=func
